1983dd170c7225dbc7dc2bca1c16d510954de9fd
Suhani-013 <126856854+Suhani-013@users.noreply.github.com>
2023-09-11 01:08:10 +0530

    card updates

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom';
 
 const Card = ({ user }) => {
   return (
-    <div className="flex h-[35vh] w-[50vh] bg-my-green dark:bg-neutral-800 flex-col items-center ">
-      <img src="https://i.pinimg.com/originals/ae/ec/c2/aeecc22a67dac7987a80ac0724658493.jpg" alt="male img" className='w-[15vh] text-center' />
+    <div className="flex h-[35vh] w-[50vh] bg-my-blue dark:bg-neutral-800 flex-col items-center ">
+      <img src="https://i.pinimg.com/originals/ae/ec/c2/aeecc22a67dac7987a80ac0724658493.jpg" alt="male img" className='w-[15vh]' />
 
-    <div className='dark:text-white'>
+    <div className='text-white flex text-center'>
       {user.desc }
     </div>
     </div>
